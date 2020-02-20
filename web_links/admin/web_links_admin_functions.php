2ccf54e155c14340c9e2a72e7df08c455cd5f1d4
Jimako <janamicek@gmail.com>
2020-02-20 12:38:15 +0100

    WL link check in progress

--- web_links/admin/web_links_admin_functions.php
+++ web_links/admin/web_links_admin_functions.php
@@ -22,10 +22,211 @@ function CloseTable() {
 
 function LinksLinkCheck() {
     $caption = _WEBLINKSADMIN. ' <span class="fa fa-angle-double-right e-breadcrumb"></span> '._VALIDATELINKS;
-    $text = 'Comming soon';
-    e107::getRender()->tablerender($caption, $text, 'web_links_index');
+ 	$sql = e107::getDb();
+ 
+	$content .= OpenTable();
+	$content .= "<div class='center'><font class=\"title\"><b>"._WEBLINKSADMIN."</b></font></div>";
+	$content .= CloseTable();
+	$content .= "<br>";
+    $content .=	OpenTable();
+	$content .= "<div class='center'><font class=\"option\"><b>"._LINKVALIDATION."</b></font></div><br>"
+	."<table width=\"100%\" align=\"center\"><tr><td colspan=\"2\" align=\"center\">"
+	."<a href=\"".UN_FILENAME_ADMIN."?op=LinksValidate&amp;cid=0&amp;sid=0\">"._CHECKALLLINKS."</a><br><br></td></tr>"
+	."<tr><td valign=\"top\"><div class='center'><b>"._CHECKCATEGORIES."</b><br>"._INCLUDESUBCATEGORIES."<br><br><font class=\"tiny\">";
+	$result = $sql->retrieve("SELECT cid, title FROM #".UN_TABLENAME_LINKS_CATEGORIES." ORDER BY title", true);
+        foreach($result AS $row) {
+			$cid = $row['cid'];
+			$title = $row['title'];
+			$transfertitle = str_replace (" ", "_", $title);
+			$content .="<a href=\"".UN_FILENAME_ADMIN."?op=LinksValidate&amp;cid=".$cid."&amp;sid=0&amp;ttitle=".$transfertitle."\">".$title."</a><br>";
+		}
+ 
+	$content .= "</font></div></td></tr></table>";
+	$content .= CloseTable();
+    e107::getRender()->tablerender($caption, $content, 'web_links_index');
 }
 
+
+function LinksValidate() {
+    $caption = _WEBLINKSADMIN. ' <span class="fa fa-angle-double-right e-breadcrumb"></span> '._CLEANLINKSDB;
+ 	$sql = e107::getDb();   
+    /* 
+      $fl = e107::getFile();  $fp =  $fl->isValidURL($row['url']);
+      - it doesn't return code to display reason and it uses get_headers without @
+    */
+	$content = OpenTable();
+	$content .= "<div class='center'><font class=\"title\"><b>"._WEBLINKSADMIN."</b></font></div>";
+	$content .= CloseTable();
+	$content .= "<br>";
+	$content .= OpenTable();
+	$cid = intval($cid);
+	$sid = intval($sid);
+	$ttitle = stripslashes(check_html($ttitle, "nohtml"));
+	$transfertitle = str_replace ("_", "", $ttitle);
+	/* Check ALL Links */
+	$content .="<table width=\"100%\" border=\"0\">";
+		if ($cid==0 && $sid==0) {
+			$content .="<tr><td colspan=\"3\"><div class='center'><b>"._CHECKALLLINKS."</b><br>"._BEPATIENT."</div><br><br></td></tr>";
+			$result = $sql->retrieve("SELECT lid, title, url FROM #".UN_TABLENAME_LINKS_LINKS." ORDER BY title", true);
+		}
+		/* Check Categories & Subcategories */
+		if ($cid!=0 && $sid==0) {
+			$content .="<tr><td colspan=\"3\"><div class='center'><b>"._VALIDATINGCAT.": ".$transfertitle."</b><br>"._BEPATIENT."</div><br><br></td></tr>";
+			$result = $sql->retrieve("SELECT lid, title, url FROM #".UN_TABLENAME_LINKS_LINKS." WHERE cid='".$cid."' ORDER BY title", true);
+		}
+		/* Check Only Subcategory */
+		if ($cid==0 && $sid!=0) {
+			$content .="<tr><td colspan=\"3\"><div class='center'><b>"._VALIDATINGSUBCAT.": ".$transfertitle."</b><br>"._BEPATIENT."</div><br><br></td></tr>";
+			$result = $sql->retrieve("SELECT lid, title, url FROM #".UN_TABLENAME_LINKS_LINKS." WHERE sid='".$sid."' ORDER BY title", true);
+		}
+	       $content .= "<tr><td bgcolor=\"$bgcolor2\" align=\"center\"><b>".LAN_STATUS."</b></td>
+           <td bgcolor=\"".$bgcolor2."\" width=\"100%\"><b>"._LINKTITLE."</b></td><td bgcolor=\"".$bgcolor2."\" align=\"center\"><b>".LAN_OPTIONS."</b></td></tr>";
+        foreach($result AS $row) {
+			$lid = $row['lid'];
+			$title = stripslashes($row['title']);
+			$url = stripslashes($row['url']);
+			$vurl = parse_url($row['url']);
+	 
+            $fp = getHttpResponseCode_using_getheaders($url, false);
+             
+            //convert code to message
+            $code = getCode($fp);
+ 
+            if ($fp == '200' ) { 
+				$content .="<tr><td align=\"center\">&nbsp;&nbsp;"._OK."&nbsp;&nbsp;</td>"
+				."<td>&nbsp;&nbsp;<a href=\"".$url."\" target=\"_blank\">".$title."</a>&nbsp;&nbsp;</td>"
+				."<td align=\"center\"><font class=\"content\">&nbsp;&nbsp;"._NONE."&nbsp;&nbsp;</font>"
+				."</td></tr>";
+		    } 
+            else{ 
+				$content .="<tr><td align=\"center\"><b>&nbsp;&nbsp;"._FAILED."&nbsp;&nbsp;</b></td>"
+				."<td>&nbsp;&nbsp;<a href=\"".$url."\" target=\"_blank\">".$title."</a>&nbsp;&nbsp;".$code." </td>"
+				."<td align=\"center\"><font class=\"content\">&nbsp;&nbsp;[ <a href=\"".UN_FILENAME_ADMIN."?op=LinksModLink&amp;lid=".$lid."\">".LAN_EDIT."</a> | <a href=\"".UN_FILENAME_ADMIN."?op=LinksDelLink&amp;lid=".$lid."\">".LAN_DELETE."</a> ]&nbsp;&nbsp;</font>"
+				."</td></tr>";
+			}		
+		}
+ 
+	$content .= "</table>";
+	$content .= CloseTable(); 
+    e107::getRender()->tablerender($caption, $content, 'web_links_index');
+}
+
+
+/**
+ * @param $url
+ * @param array $options
+ * @return string
+ * @throws Exception
+ */
+function getCode($returnedStatusCode) {
+ 
+    // list of HTTP status codes
+    $httpStatusCodes = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Payload Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        599 => 'Network Connect Timeout Error'
+    );
+    if (array_key_exists($returnedStatusCode, $httpStatusCodes)) {
+        return "Code: {$returnedStatusCode} - Definition: {$httpStatusCodes[$returnedStatusCode]}";
+    } else {
+        return "Url does not exist";
+    } 
+    return   $httpStatusCodes[$returnedStatusCode];
+ 
+}
+ 
+
+    function getHttpResponseCode_using_getheaders($url, $followredirects = false){
+        // returns string responsecode, or false if no responsecode found in headers (or url does not exist)
+        // NOTE: could potentially take up to 0-30 seconds , blocking further code execution (more or less depending on connection, target site, and local timeout settings))
+        // if $followredirects == false: return the FIRST known httpcode (ignore redirects)
+        // if $followredirects == true : return the LAST  known httpcode (when redirected)
+        if(! $url || ! is_string($url)){
+            return false;
+        }
+        $headers = @get_headers($url);
+        if($headers && is_array($headers)){
+ 
+            if($followredirects){
+                // we want the the last errorcode, reverse array so we start at the end:
+                $headers = array_reverse($headers);
+            }
+            foreach($headers as $hline){
+                // search for things like "HTTP/1.1 200 OK" , "HTTP/1.0 200 OK" , "HTTP/1.1 301 PERMANENTLY MOVED" , "HTTP/1.1 400 Not Found" , etc.
+                // note that the exact syntax/version/output differs, so there is some string magic involved here
+                if(preg_match('/^HTTP\/\S+\s+([1-9][0-9][0-9])\s+.*/', $hline, $matches) ){// "HTTP/*** ### ***"
+                    $code = $matches[1];
+                    return $code;
+                }
+            }
+            // no HTTP/xxx found in headers:
+            return false;
+        }
+        // no headers :
+        return false;
+    }
+    
 function LinksCleanVotes() {
     $caption = _WEBLINKSADMIN. ' <span class="fa fa-angle-double-right e-breadcrumb"></span> '._CLEANLINKSDB;
     $text = 'Comming soon';
@@ -243,7 +444,7 @@ function LinksEditBrokenLinks($lid) {
 	$description = stripslashes($row['description']);
 	$modifysubmitter = $row['modifysubmitter'];
 	$result2 = $sql->retrieve("SELECT name, email, hits FROM #".UN_TABLENAME_LINKS_LINKS." WHERE lid='".$lid."'", true);
-    print_a($row['url']);
+ 
 	$row2 = $result2[0];
  
 	$name = $row2['name'];
@@ -376,4 +577,20 @@ function LinksChangeModRequests($requestid) {
  
  	$sql->gen("DELETE FROM #".UN_TABLENAME_LINKS_MODREQUEST." WHERE requestid=".$requestid);
     Header("Location: ".UN_FILENAME_ADMIN."?op=LinksListModRequests");
+}
+
+
+function LinksDelLink($lid) {
+	$sql = e107::getDb();
+	$lid = intval($lid);
+	$sql->gen("DELETE FROM #".UN_TABLENAME_LINKS_LINKS." WHERE lid='".$lid."'");
+	$query = "SELECT COUNT(*) AS numrows FROM #".UN_TABLENAME_LINKS_MODREQUEST." WHERE lid='".$lid."'";
+	$result = $sql->gen($query);
+	$row = $sql->fetch($result);
+ 
+	$numrows = $row['numrows'];
+		if ($numrows>0) {
+			$sql->gen("DELETE FROM #".UN_TABLENAME_LINKS_MODREQUEST." WHERE lid='".$lid."'");
+		}
+	Header("Location: ".UN_FILENAME_ADMIN."?op=Links");
 }
